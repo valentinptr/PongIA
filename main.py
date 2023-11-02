a3bd07073449c8885ccf8080cb89c8399e8b33f9
valentinptr <valentin.ptr@icloud.com>
2023-11-02 18:47:54 +0100

    Animation des pallets

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ fpsClock = pygame.time.Clock()
 
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 800
+CURSOR_SPEED = 200
 
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -14,10 +15,12 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
+cursor_x = WINDOW_WIDTH / 2 - 50
+
 engine = Engine()
 ball = Ball(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
-pallet_j1 = Pallets(WINDOW_WIDTH / 2 - 50, 40)
-pallet_j2 = Pallets(WINDOW_WIDTH / 2 - 50, WINDOW_HEIGHT - 40)
+pallet_j1 = Pallets(cursor_x, 40)
+pallet_j2 = Pallets(cursor_x, WINDOW_HEIGHT - 40)
 
 while running:
     for event in pygame.event.get():
@@ -26,13 +29,21 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 engine.dropBall(ball.position.x, ball.position.y)
-                engine.dropPallets(pallet_j1.position.x, pallet_j1.position.y)
-                engine.dropPallets(pallet_j2.position.x, pallet_j2.position.y)
 
     engine.update(dt)
     engine.drawBackground(screen)
     engine.drawBall(screen)
     engine.drawPallets(screen)
+
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_LEFT]:
+        cursor_x -= CURSOR_SPEED * dt
+    if keys_pressed[pygame.K_RIGHT]:
+        cursor_x += CURSOR_SPEED * dt
+
+    pallet_j1.position.x = cursor_x
+    pallet_j1.draw(screen)
+    pallet_j2.draw(screen)
 
     pygame.display.flip()
     dt = clock.tick(FPS) / 1000
